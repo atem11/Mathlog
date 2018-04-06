02e9d82486ef5f956171222754db78750b6f9c35
Artem Abramov <atem199700@gmail.com>
2018-04-06 20:57:40 +0300

    "add HW1"

--- src/axioms/Equaler.java
+++ src/axioms/Equaler.java
@@ -1,4 +1,44 @@
 package axioms;
 
+import operations.Expression;
+import operations.Variable;
+
+import java.util.HashMap;
+
 public class Equaler {
+    private HashMap<Variable, Expression> axiomVars;
+    private Expression equals;
+
+    public Equaler(Expression pattern) {
+        this.equals = pattern;
+        axiomVars = new HashMap<>();
+    }
+
+    private boolean walkAndCheck(Expression fromAxiom, Expression toCheck) {
+        if (fromAxiom instanceof Variable) {
+            Variable axiomVariable = (Variable)fromAxiom;
+            if (!axiomVars.containsKey(axiomVariable)) {
+                axiomVars.put(axiomVariable, toCheck);
+                return true;
+            } else {
+                return axiomVars.get(axiomVariable).equals(toCheck);
+            }
+        }
+        if (!fromAxiom.getSymbol().equals(toCheck.getSymbol())) {
+            return false;
+        }
+        Expression[] axiomChildren = fromAxiom.getChildren();
+        Expression[] toCheckChildren = toCheck.getChildren();
+        for (int i = 0; i < axiomChildren.length; i++) {
+            if (!walkAndCheck(axiomChildren[i], toCheckChildren[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean check(Expression other) {
+        axiomVars.clear();
+        return walkAndCheck(equals, other);
+    }
 }
